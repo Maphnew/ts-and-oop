3c91798fbe0b7724dc8727b8c5eb662fca94756c
maphnew <zcm3118@gmail.com>
2022-04-28 22:38:16 +0900

    14.8 이미지 컴포넌트 만들기 💡

--- motion/dist/app.js
+++ motion/dist/app.js
@@ -1,8 +1,11 @@
-import { PageComponent } from "./components/page.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { PageComponent } from "./components/page/page.js";
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
+        const image = new ImageComponent("image title", "https://picsum.photos/600/300");
         this.page.attachTo(appRoot);
+        image.attachTo(appRoot, "beforeend");
     }
 }
 new App(document.querySelector(".document"));
--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,10 +1,14 @@
-import { PageComponent } from "./components/page.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { PageComponent } from "./components/page/page.js";
 
 class App {
   private readonly page: PageComponent;
+
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
+    const image = new ImageComponent("image title", "https://picsum.photos/600/300");
     this.page.attachTo(appRoot);
+    image.attachTo(appRoot, "beforeend");
   }
 }
 
