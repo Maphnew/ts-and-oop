d684d778124ebe71d88d59c8cc2a0070264e698d
maphnew <zcm3118@gmail.com>
2022-04-30 17:58:47 +0900

    14.14 모든 섹션들 구현하기💡 (유튜브 URL 만들기편)

--- motion/src/components/page/item/video.ts
+++ motion/src/components/page/item/video.ts
@@ -11,6 +11,16 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
     const titleElement = this.element.querySelector(".video__title")! as HTMLHeadingElement;
     const srcElement = this.element.querySelector(".video__iframe")! as HTMLIFrameElement;
     titleElement.textContent = title;
-    srcElement.src = url;
+    srcElement.src = this.convertToEmbeddedURL(url);
+  }
+  private convertToEmbeddedURL(url: string): string {
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
   }
 }
