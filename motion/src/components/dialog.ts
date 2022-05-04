3aa8ab8e7127d30fb68d473a0904771109f87130
maphnew <zcm3118@gmail.com>
2022-05-04 20:50:21 +0900

    14.23 드디어 텅빈 다이얼로그 만들기 💡

---
+++ motion/src/components/dialog.ts
@@ -0,0 +1,39 @@
+import { BaseComponent, Component } from "./base.js";
+import { Composable } from "./page/page.js";
+
+type OnCloseListener = () => void;
+type OnSubmitListener = () => void;
+
+export class InputDialog extends BaseComponent<HTMLElement> implements Composable {
+  closeListener?: OnCloseListener;
+  submitListener?: OnSubmitListener;
+  constructor() {
+    super(`<section class="dialog">
+        <div class="dialog__container">
+          <button class="close">&times;</button>
+          <div class="dialog__body"></div>
+          <button class="dialog__submit">ADD</button>
+        </div>
+      </section>`);
+    const close = this.element.querySelector(".close")! as HTMLButtonElement;
+    const submit = this.element.querySelector(".dialog__submit")! as HTMLButtonElement;
+    close.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
+    submit.onclick = () => {
+      this.submitListener && this.submitListener();
+    };
+  }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
+  }
+  setOnSubmitListener(listener: OnSubmitListener) {
+    this.submitListener = listener;
+  }
+
+  addChild(child: Component): void {
+    const body = this.element.querySelector(".dialog__body")! as HTMLElement;
+    child.attachTo(body);
+  }
+}
